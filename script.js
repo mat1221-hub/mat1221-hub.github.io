336d4db1d83113370e1c398f8151c0fa08141971
Mulat Ayinet <57259262+mat1221-hub@users.noreply.github.com>
2024-07-07 20:23:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const colors = [
-        "#FF5733", "#33FF57", "#3357FF", "#FF33A6", "#FF8333",
-        "#A633FF", "#33FFF5", "#FF5733", "#FF333F", "#33FF9F"
-    ];
-
-    let currentColorIndex = 0;
-
-    function changeBackgroundColor() {
-        document.body.style.backgroundColor = colors[currentColorIndex];
-        currentColorIndex = (currentColorIndex + 1) % colors.length;
-    }
-
-    changeBackgroundColor();
-    setInterval(changeBackgroundColor, 60000);
-});
-
-function toggleMenu() {
-    const menu = document.querySelector('nav ul');
-    menu.classList.toggle('show-menu');
+// script.js
+function changeBackgroundColorToGray() {
+    document.body.style.backgroundColor = 'gray';
 }
